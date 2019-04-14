27af881a8bfd953f6eb2eb7e4ae6920698530fa4
Patrick Balian <goldenboycoder@gmail.com>
2019-04-14 23:51:33 +0300

    patrick Balian Added an Activity for the Host Screen , where can input the details of the event then click create which will later plug in a fragment where the QR code will be shown . Or he can choose to only enter the name of the event/nothing and an instant event should be created. Only  the UI was done .

---
+++ app/src/main/java/com/example/attendance/QR_Code_host.java
@@ -0,0 +1,13 @@
+package com.example.attendance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QR_Code_host extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr__code_host);
+    }
+}
