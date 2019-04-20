34eaac4a2316993b88b30a977d045242285251df
Patrick Balian <goldenboycoder@gmail.com>
2019-04-21 00:41:00 +0300

    Patrick Balian Added a Login Screen with logo and cool UI

--- app/src/main/java/com/example/attendance/MainActivity.java
+++ app/src/main/java/com/example/attendance/MainActivity.java
@@ -1,17 +1,44 @@
 package com.example.attendance;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText username;
+    EditText password;
+    public final String Preference="privateS";
+    public final String Authentication="isLogedin";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*ActionBar actionBar=getSupportActionBar();
-        actionBar.hide();*/
+        username=findViewById(R.id.edtUsername);
+        password=findViewById(R.id.editPassword);
+    }
+    public void studentAcess(View v){
+        Intent intent=new Intent(getApplicationContext(),StudentHomeScreen.class);
+        startActivity(intent);
+    }
+
+    public void loginAdmin(View v){
+        if(username.getText().toString().equals("admin")&&password.getText().toString().equals("admin")){
+            Intent intent=new Intent(getApplicationContext(),AdminHomeScreen.class);
+            SharedPreferences sharedPreferences=getApplicationContext().getSharedPreferences(Preference, Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor=sharedPreferences.edit();
+            editor.putBoolean(Authentication,true);
+            editor.commit();
+            startActivity(intent);
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"Failed to login retry",Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/attendance/StudentHomeScreen.java
+++ app/src/main/java/com/example/attendance/StudentHomeScreen.java
@@ -3,11 +3,11 @@ package com.example.attendance;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class QR_Code_host extends AppCompatActivity {
+public class StudentHomeScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_qr__code_host);
+        setContentView(R.layout.activity_student_home_screen);
     }
 }
