fd12ab5da2fda1a93e645e9a1dd32aae274db6e6
Nizar1999 <daouk.mohamadnizar@gmail.com>
2019-04-28 15:04:55 +0300

    Improved Readability: Clean Code/Fix Grammar

--- app/src/main/java/com/example/attendance/MainActivity.java
+++ app/src/main/java/com/example/attendance/MainActivity.java
@@ -14,16 +14,16 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     EditText username;
     EditText password;
-    public final String Preference="privateS";
-    public final String Authentication="isLogedin";
+    public final String Preference = "privateS";
+    public final String Authentication = "isLoggedin";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
-        username=findViewById(R.id.edtUsername);
-        password=findViewById(R.id.editPassword);
-        SharedPreferences sharedPreferences=getApplicationContext().getSharedPreferences(Preference, Context.MODE_PRIVATE);
+        username = findViewById(R.id.edtUsername);
+        password = findViewById(R.id.editPassword);
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(Preference, Context.MODE_PRIVATE);
         if(sharedPreferences.contains(Authentication)) {
             if (sharedPreferences.getBoolean(Authentication, false)) {
                 Intent intent = new Intent(getApplicationContext(), HomeScreen.class);
@@ -32,24 +32,24 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-    public void studentAcess(View v){
-        Intent intent=new Intent(getApplicationContext(),HomeScreen.class);
+    public void studentAccess(View v){
+        Intent intent = new Intent(getApplicationContext(), HomeScreen.class);
         intent.putExtra("admin",false);
         startActivity(intent);
     }
 
     public void loginAdmin(View v){
-        if(username.getText().toString().equals("admin")&&password.getText().toString().equals("admin")){
-            Intent intent=new Intent(getApplicationContext(),HomeScreen.class);
+        if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+            Intent intent = new Intent(getApplicationContext(), HomeScreen.class);
             SharedPreferences sharedPreferences=getApplicationContext().getSharedPreferences(Preference, Context.MODE_PRIVATE);
-            SharedPreferences.Editor editor=sharedPreferences.edit();
-            editor.putBoolean(Authentication,true);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean(Authentication, true);
             editor.commit();
             intent.putExtra("admin",true);
             startActivity(intent);
         }
         else{
-            Toast.makeText(getApplicationContext(),"Failed to login retry",Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(),"Failed to login retry", Toast.LENGTH_SHORT).show();
         }
     }
 }
